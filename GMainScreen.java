79d2e1b7b1dcadb960ec4e554d6c825ba046a2ed
z1102222 <129866130+z1102222@users.noreply.github.com>
2023-04-13 08:17:46 -0500

    hi

--- GMainScreen.java
+++ GMainScreen.java
@@ -3,11 +3,69 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class GMainScreen extends JFrame{
+    final int WIDTH = 1920;
+    final int HEIGHT = 1080;
+    private GUpper gUpper;
+    private GLower gLower;
+    public static void main (String[] args){
+        JFrame main = new GMainScreen();
+    }
+
     GMainScreen(){
         super();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(WIDTH,HEIGHT);
+        setTitle("COMP SCI, THE CARD GAME, presented by team (a)MiRiCa");
+        GridLayout jfLay = new GridLayout(2, 0);
+        setLayout(jfLay);
+
+        gUpper = new GUpper();
+        add(gUpper);
+
+        gLower = new GLower();
+        add(gLower);
+
+        //End
+        setVisible(true);
     }
-    
-    public void paintComponent(Graphics g){
-        
+
+    private class GUpper extends GPanel{
+        GUpper() {
+            super(WIDTH, HEIGHT / 2, 1, 3, Color.gray);
+            JLabel fil = new JLabel();
+            add(fil);
+
+            JLabel title = new JLabel("Comp Sci the Game");
+            title.setFont(new Font("Georgia", Font.BOLD, 25));
+            title.setForeground(Color.green);
+            title.setAlignmentX(CENTER_ALIGNMENT);
+            add(title);
+
+            JLabel fil2 = new JLabel();
+            add(fil2);
+        }
+    }
+
+    private class GLower extends GPanel{
+
+        GLower() {
+            super(WIDTH, HEIGHT / 2, 2, 1, Color.white);
+            JButton start = new JButton("Start");
+            start.setForeground(Color.CYAN);
+            start.setBackground(Color.ORANGE);
+            start.addActionListener(new stGame());
+            add(start);
+        }
+    }
+    class stGame implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            remove(gUpper);
+            remove(gLower);
+            setLayout(new FlowLayout());
+            GBattleScreen batt = new GBattleScreen();
+            add(batt);
+            revalidate();
+            repaint();
+        }
     }
 }
