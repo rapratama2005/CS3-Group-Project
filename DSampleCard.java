1928c44318658db59deb45d2780e961ad1469395
I'mNotRisky <rapratama2005@gmail.com>
2023-04-18 08:35:01 -0500

    Superattack work

--- DSampleCard.java
+++ DSampleCard.java
@@ -2,26 +2,34 @@
 public class DSampleCard extends DsuperCard{
 
     public DSampleCard() {
-        super(10, 2, 10, 2, "Sample Man", "Sample Attack", "Sample Super Attack", "Sample Poison Attack", 10);
+        super(10, 2, 10, 2, "Sample Man", "Sample Attack", "Sample Super Attack", "Sample Poison Attack", 6);
     }
 
     @Override
     public String atk(DsuperCard target) {
         target.hurt(super.getAtk());
-        return (this.getName() + " used " + this.getAName() + " on " + target.getName() + " for " + this.getAtk() + "damage.");
+        progSup(super.getAtk());
+        return (this.getName() + " used " + this.getAName() + " on " + target.getName() + " for " + this.getAtk() + " damage.");
     }
 
     @Override
     public String bAtk(DsuperCard target) {
         DConditionPoison samplePoison = new DConditionPoison(3, "Poison", 1);
         target.addConditions(samplePoison);
+        progSup(5);
         return (this.getName() + " used " + this.getBName() + " on " + target.getName() + ".");
     }
 
     @Override
     public String sAtk(DsuperCard target) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sAtk'");
+        // throw new UnsupportedOperationException("Unimplemented method 'sAtk'");
+        if (!super.checkSup()){
+            return(super.sAtk(target));
+        } else {
+            target.hurt(super.getAtk()*3);
+            return(super.sAtk(target));
+        }
     }
     
 }
