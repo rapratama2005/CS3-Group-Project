211c28963989dc4b6e2b292b59edbd04c06675f7
z1102222 <129866130+z1102222@users.noreply.github.com>
2023-04-20 08:48:46 -0500

    h

--- GBattleScreen2.java
+++ GBattleScreen2.java
@@ -3,10 +3,11 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class GBattleScreen2 extends JFrame{
+    private JPanel card1,card2,card3,card4,hand1,hand2;
     GBattleScreen2(){
         super();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new GridLayout(2,2));
-        
+        card1,card2,card3,card4 = new JPanel();
     }
 }
