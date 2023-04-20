7c131158856f575cd16e5b987232bfbbf38f467e
z1102222 <129866130+z1102222@users.noreply.github.com>
2023-04-20 09:01:49 -0500

    Update GBattleScreen2.java

--- GBattleScreen2.java
+++ GBattleScreen2.java
@@ -18,18 +18,46 @@ public class GBattleScreen2 extends JFrame{
         card1.add(c1);
         card1P = new JPanel();
         c1.add(card1P);
-        D1 = new JTextArea();
-        c1.add(D1);
+        D1 = new JTextArea(10,15);
+        JScrollPane s1 = new JScrollPane(D1);
+        c1.add(s1);
         add(card1);
 
         card2 = new JPanel();
         card2.setLayout(new GridLayout(1,2));
+        JPanel c2 = new JPanel();
+        c2.setLayout(new GridLayout(2,1));
+        card2.add(c1);
+        card1P = new JPanel();
+        c1.add(card1P);
+        D1 = new JTextArea(10,15);
+        JScrollPane s1 = new JScrollPane(D1);
+        c1.add(s1);
+        add(card1);
 
         card3 = new JPanel();
         card3.setLayout(new GridLayout(1,2));
+        JPanel c1 = new JPanel();
+        c1.setLayout(new GridLayout(2,1));
+        card1.add(c1);
+        card1P = new JPanel();
+        c1.add(card1P);
+        D1 = new JTextArea(10,15);
+        JScrollPane s1 = new JScrollPane(D1);
+        c1.add(s1);
+        add(card1);
         
         card4 = new JPanel();
         card4.setLayout(new GridLayout(1,2));
+        JPanel c1 = new JPanel();
+        c1.setLayout(new GridLayout(2,1));
+        card1.add(c1);
+        card1P = new JPanel();
+        c1.add(card1P);
+        D1 = new JTextArea(10,15);
+        JScrollPane s1 = new JScrollPane(D1);
+        c1.add(s1);
+        add(card1);
         
         
     }
