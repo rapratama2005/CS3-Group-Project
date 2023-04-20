4fe2d2cb405d6999ac19ed5a527d2fc4b471211d
z1102222 <129866130+z1102222@users.noreply.github.com>
2023-04-20 08:59:30 -0500

    Update GBattleScreen2.java

--- GBattleScreen2.java
+++ GBattleScreen2.java
@@ -4,12 +4,23 @@ import javax.swing.*;
 
 public class GBattleScreen2 extends JFrame{
     private JPanel card1,card2,card3,card4,hand1,hand2;
+    private JPanel card1P,card2P,card3P,card4P;
+    private JTextArea D1,D2,D3,D4;
+
     GBattleScreen2(){
         super();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new GridLayout(2,2));
         card1 = new JPanel();
         card1.setLayout(new GridLayout(1,2));
+        JPanel c1 = new JPanel();
+        c1.setLayout(new GridLayout(2,1));
+        card1.add(c1);
+        card1P = new JPanel();
+        c1.add(card1P);
+        D1 = new JTextArea();
+        c1.add(D1);
+        add(card1);
 
         card2 = new JPanel();
         card2.setLayout(new GridLayout(1,2));
