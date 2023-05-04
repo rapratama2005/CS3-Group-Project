ceb1e3f735dbad031c701d382e249278b6d60092
z1102222 <129866130+z1102222@users.noreply.github.com>
2023-05-04 08:59:02 -0500

    Update GCardSide2.java

--- GCardSide2.java
+++ GCardSide2.java
@@ -52,7 +52,7 @@ public class GCardSide2 extends GPanel{
                 confirm.setForeground(Color.yellow);
                 confirm.setBorder(border);
                 pan.add(confirm);
-                confirm.addActionListener(new confirmCard(selected,1));
+                confirm.addActionListener(new confirmCard(selected));
             }
             tex.setText(deck.cardID(selected).getDesc(0));
         }
@@ -60,10 +60,8 @@ public class GCardSide2 extends GPanel{
 
     class confirmCard implements ActionListener{
         private String sel;
-        private int i;
-        public confirmCard(String s, int num){
+        public confirmCard(String s){
             sel=s;
-            i = num;
         }
         public void setS(String s){
             sel = s;
@@ -71,62 +69,21 @@ public class GCardSide2 extends GPanel{
         public void actionPerformed(ActionEvent e){
             DsuperCard car = deck.cardID(sel);
             card = car;
+            changeImage(car.getImgSource());
             //field.add(car);
             pan.remove(confirm);
             pan.remove(box);
             tex.setText("");
-            /*
-                changeImage(1,car.getImgSource());
-                atks.get(0).setText(car.getAName());
-                atks.get(1).setText(car.getBName());
-                currH.get(0).setText("  "+car.getHealth());
-                currA.get(0).setText(""+car.getAtk()+"  ");
-                supBar1.setMB(car.getSupReq());
-                supBar1.setT(car.getDesc(3));
-            }
-            else if(i==2){
-                p2.remove(confirms.get(2));
-                confirms.remove(2);
-                p2.remove(box2);
-                D2.setText("");
-                changeImage(2,car.getImgSource());
-                atks.get(2).setText(car.getAName());
-                atks.get(3).setText(car.getBName());
-                currH.get(1).setText("  "+car.getHealth());
-                currA.get(1).setText(""+car.getAtk()+"  ");
-                supBar2.setMB(car.getSupReq());
-                supBar2.setT(car.getDesc(3));
-            }
-            else if (i==3){
-                p3.remove(confirms.get(3));
-                confirms.remove(3);
-                p3.remove(box3);
-                D3.setText("");
-                changeImage(3,car.getImgSource());
-                atks.get(4).setText(car.getAName());
-                atks.get(5).setText(car.getBName());
-                currH.get(2).setText("  "+car.getHealth());
-                currA.get(2).setText(""+car.getAtk()+"  ");
-                supBar3.setMB(car.getSupReq());
-                supBar3.setT(car.getDesc(3));
-            }
-            else if (i==4){
-                p4.remove(confirms.get(4));
-                confirms.remove(4);
-                p4.remove(box4);
-                D4.setText("");
-                changeImage(4,car.getImgSource());
-                atks.get(6).setText(car.getAName());
-                atks.get(7).setText(car.getBName());
-                currH.get(3).setText("  "+car.getHealth());
-                currA.get(3).setText(""+car.getAtk()+"  ");
-                supBar4.setMB(car.getSupReq());
-                supBar4.setT(car.getDesc(3));
-            }
-            turn++;
-            System.out.println(field);
         }
-        */
+        public void changeImage(String fileN){
+            ImageIcon a = new ImageIcon(fileN);
+            pic = new JLabel(a);
+            pan.add(pic);
+            revalidate();
+            repaint();
+            }
     }
-    
+    public DsuperCard getCard(){
+        return card;
+    }
 }
