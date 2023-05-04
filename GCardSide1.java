d8e82441887d7a08896a63b19c7706a1db18447e
I'mNotRisky <rapratama2005@gmail.com>
2023-05-04 09:00:49 -0500

    a

--- GCardSide1.java
+++ GCardSide1.java
@@ -1,11 +1,15 @@
 import java.awt.Color;
 
 public class GCardSide1 extends GPanel{
+    GCardStats hp = new GCardStats("Health", 1);
+    GCardStats at = new GCardStats("Base Attack", 1);
+    GPanel hpAtk = new GPanel(WIDTH, HEIGHT, 1, 2, Color.white);
 
     GCardSide1() {
         super(WIDTH, HEIGHT, 5, 1, Color.white);
-        
-        //TODO Auto-generated constructor stub
+        hpAtk.add(hp);
+        hpAtk.add(at);
+        add(hpAtk);
     }
     
 }
---
+++ GCardStats.java
@@ -0,0 +1,29 @@
+import java.awt.Color;
+
+import javax.swing.JLabel;
+
+public class GCardStats extends GPanel{
+    String name;
+    int val;
+    JLabel nameL = new JLabel();
+    JLabel valL = new JLabel();
+    GCardStats(String n, int v) {
+        super(WIDTH, HEIGHT, 1, 2, Color.white);
+        name = n;
+        val = v;
+        nameL.setText(name);
+        nameL.setVisible(true);
+        nameL.setBackground(Color.BLACK);
+        valL.setText(""+val);
+        valL.setVisible(true);
+        
+    }
+
+    void refreshVal(int v){
+        val = v;
+        valL.setText(""+val);
+        valL.setVisible(true);
+
+    }
+    
+}
